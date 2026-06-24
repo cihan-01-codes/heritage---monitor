95037acc5b5120470df0abd367ebb0507e1aba21
Cihan <daudiissa369@gmail.com>
2026-06-24 11:33:51 +0300

    Fixed readings URL conflicts, cleaned up CSS classes

--- heritage_core/urls.py
+++ heritage_core/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from sensors import views as sensor_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sensor-data/', include('sensors.urls')),
-    path('readings/', include('sensors.urls')),
+    path('readings/', sensor_views.sensor_readings_list, name='sensor_readings_list'),
+    path('readings/<int:building_id>/', sensor_views.sensor_readings_by_building, name='sensor_readings_by_building'),
     path('', include('accounts.urls')),
     path('', include('buildings.urls')),
     path('', include('alerts.urls')),
